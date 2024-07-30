82135bf5e7cd4e27e1680245c2d9a5cec2a585e3
unknown <raz.e@sightbit.com>
2024-07-30 14:10:24 +0300

    finalizing user dashboard

--- modules/users/controllers/userDashboard.js
+++ modules/users/controllers/userDashboard.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const userDashboard = async (req, res) => {
 
     const usersModel = mongoose.model("users");
+    const transactionModel = mongoose.model("transactions");
 
     const getUser = await usersModel
     .findOne({
@@ -10,13 +11,17 @@ const userDashboard = async (req, res) => {
     })
     .select("-password") // we dont want to send the password in the response or .select("name balance email"); // option 2
     
-
-
-    console.log(req.user);
+    const transactions = await transactionModel
+    .find({
+        user_id: req.user._id
+    })
+    .sort("-createdAt")
+    .limit(5);
 
     res.status(200).json({
         status: "success",
-        data: getUser
+        data: getUser,
+        transactions // this equal to transactions: transactions
     });
 };
 
