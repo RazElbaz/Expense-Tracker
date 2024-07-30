fc4e5deaea67c61238ccad768cb7cccb01062834
unknown <raz.e@sightbit.com>
2024-07-30 15:37:54 +0300

    reset password using reset code

--- modules/users/users.routes.js
+++ modules/users/users.routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
+
+const auth = require("../../middleware/auth");
+
 const register = require("./controllers/register");
 const login = require("./controllers/login");
 const userDashboard = require("./controllers/userDashboard");
-const auth = require("../../middleware/auth");
 const forgotPassword = require("./controllers/forgotPassword");
+const resetPassword = require("./controllers/resetPassword");
 
 const userRoutes = express.Router();
 
@@ -12,6 +15,7 @@ userRoutes.post("/register", register);
 userRoutes.post("/login", login);
 
 userRoutes.post("/forgotpw", forgotPassword);
+userRoutes.post("/resetpw", resetPassword);
 
 userRoutes.use(auth);
 
