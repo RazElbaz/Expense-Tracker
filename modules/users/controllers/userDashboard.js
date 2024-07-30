b746949779af570599d40ad45977d1d393f81a39
unknown <raz.e@sightbit.com>
2024-07-30 12:16:56 +0300

    added user dashboard

--- modules/users/controllers/userDashboard.js
+++ modules/users/controllers/userDashboard.js
@@ -1,9 +1,22 @@
-const userDashboard = (req, res) => {
+const mongoose = require("mongoose");
+
+const userDashboard = async (req, res) => {
+
+    const usersModel = mongoose.model("users");
+
+    const getUser = await usersModel
+    .findOne({
+        _id: req.user._id
+    })
+    .select("-password") // we dont want to send the password in the response or .select("name balance email"); // option 2
+    
+
 
     console.log(req.user);
 
     res.status(200).json({
-        status: "Hello from userDashboard"
+        status: "Hello from userDashboard",
+        data: getUser
     });
 };
 
