e91e2d679c4d477277073012ddd936b6bea4e18f
unknown <raz.e@sightbit.com>
2024-07-30 10:53:48 +0300

    added a hash to the password

--- modules/users/controllers/register.js
+++ modules/users/controllers/register.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
 
 const register = async (req, res) => {
     const usersModel = mongoose.model("users");
@@ -21,10 +23,12 @@ const register = async (req, res) => {
 
     if(getDuplicateEmail) throw "This email already exist!";
 
+    const hashedPassword = await bcrypt.hash(password, 12);
+
     await usersModel.create({
         name: name,
         email: email,
-        password: password,
+        password: hashedPassword,
         balance: balance
     });
     
